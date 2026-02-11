4084c1ace876fca6004df033dfebe38d3990be36
Monica Washington <monica.lynnwashington@gmail.com>
2026-02-10 21:39:25 -0700

    Completed Methods Exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,70 @@
-﻿namespace MethodsExercise
+﻿using System.Globalization;
+
+namespace MethodsExercise
 {
     public class Program
     {
+        public static void Madlib()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}! What is your favorite color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine($"What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+            Console.WriteLine($"What is your favorite band?");
+            string favBand = Console.ReadLine();
+            
+            Console.WriteLine($"One day, {userName} was walking in the park wearing a {favColor} blouse. {userName} saw a {favAnimal} listening to {favBand}.");
+        }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
+        }
+
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                //sum = sum + number;
+                sum += number;
+            }
+            return sum; 
+
+            {
+                
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          //Madlib();
+          //Console.WriteLine(Add(2,4));
+          //int addedNums = Add (2, 4);
+          //Console.WriteLine(Subtract(10, 2));
+          //Console.WriteLine(Multiply(10, 2));
+          //Console.WriteLine(Divide(20, 10));
+          Console.WriteLine(Sum(1, 1, 1, 1, 1));
+
+
         }
     }
 }
